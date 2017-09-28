b930cb5cedade2d7a699a52a7a074eb197d93c97
timpuyda <31927275+timpuyda@users.noreply.github.com>
2017-09-28 14:49:30 +0300

    Delete lab1_task4.m

--- lab1_task4.m
+++
@@ -1,7 +0,0 @@
-s= log(3);
-a=degtorad(44);
-c1=5*sin(degtorad(35))*sqrt(s^3*cos(degtorad(36))/((pi^3)*tan(a)))
-
-s= log(18/25);
-a=7/12*pi;
-c2=5*sin(degtorad(35))*sqrt(s^3*cos(degtorad(36))/((pi^3)*tan(a)))
